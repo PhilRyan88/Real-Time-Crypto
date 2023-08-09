b6da6a5bfba3a17e35ec6f72b7c6011a86cf0198
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-08-10 00:45:18 +0530

    Cyptocurrency market trend

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import Crypto from "./Crypto";
+import { Routes, Route } from "react-router-dom";
+//import Coin from "./Coin";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+function App() {
+  const [isWalletInstalled, setIsWalletInstalled] = useState(false);
+  const navigate = useNavigate();
+  const [account, setAccount] = useState(null);
+  useEffect(() => {
+    if (window.ethereum) {
+      setIsWalletInstalled(true);
+    }
+  }, []);
+  async function connectWallet() {
+    window.ethereum
+      .request({
+        method: "eth_requestAccounts",
+      })
+      .then((accounts) => {
+        setAccount(accounts[0]);
+      })
+      .catch((error) => {
+        alert("Oops! Something went wrong bro :(");
+      });
+  }
+  if (account === null) {
+    return (
+      <div>
+        <h3>Welcome to the RealTimeCrypto</h3>
+        {isWalletInstalled ? (
+          <button onClick={connectWallet}>Connect Wallet</button>
+        ) : (
+          <p>Install any crypto wallet to continue</p>
+        )}
+      </div>
+    );
+  }
+  return (
+    <div>
+      <span>Connected as: {account}</span>
+      <button
+        onClick={() => {
+          navigate("Trend");
+        }}
+      >
+        Continue
+      </button>
+      <Routes>
+        <Route path="Trend" element={<Crypto />} />
+      </Routes>
+    </div>
+  );
+}
+export default App;
---
+++ src/Coin.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Coin.css";
+
+const Coin = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+
+  image,
+  priceChange,
+}) => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto" />
+          <h1>{name}</h1>
+          <h1 className="coin-symbol">{symbol}</h1>
+        </div>
+        <div className="coin-data">
+          <h4 className="coin-price">₹{price}</h4>
+
+          {priceChange < 0 ? (
+            <h3 className="coin-percent red">{priceChange.toFixed(2)}%</h3>
+          ) : (
+            <h3 className="coin-percent green">{priceChange.toFixed(2)}%</h3>
+          )}
+
+          <p className="coin-marketcap">
+            Mkt Cap: ₹{marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
